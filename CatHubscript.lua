13a0521985c7d10bd617a6c9d395b39992ac67ee
CatHub <165255694+CatHub027@users.noreply.github.com>
2024-05-22 00:30:04 +0700

    Update CatHubscript.lua

--- CatHubscript.lua
+++ CatHubscript.lua
@@ -169,7 +169,7 @@ Tab:AddButton({
 Tab:AddButton({
     Name = "Fly Gui v3 FE",
     Callback = function()
-            loadstring(game:HttpGet('https://raw.githubusercontent.com/Cataihub/FLYGUIV3/main/Flyguiv3'))()
+            loadstring(game:HttpGet('https://raw.githubusercontent.com/Cataihub/FLYGUIV3/main/Flyguiv3.lua'))()
     end    
 })
 
