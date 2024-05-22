08c6073b9520e98cd40356eafdfb2509821cb12c
CatHub <165255694+CatHub027@users.noreply.github.com>
2024-05-22 20:28:52 +0700

    Update CatHubscript.lua

--- CatHubscript.lua
+++ CatHubscript.lua
@@ -11,6 +11,7 @@ StarterGui:SetCore("SendNotification", {
     Duration = 4
 })
 
+
 --//tab
 local Tab = Window.NewTab("Main")
 
@@ -18,3 +19,15 @@ local Tab = Window.NewTab("Main")
 --//section
 local Section = Tab.NewSection("Main Section")
 
+--// Button
+local Button = Section.NewButton("Fly Gui v3",function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro69Yes/sussy-Script/main/SecuredFlyGuiv3.lua'))()
+end)
+
+
+--//tab
+local Tab = Window.NewTab("Blade Ball 🔴")
+
+
+--//section
+local Section = Tab.NewSection("Scripts | Balde ball")
