f33704d0202e43ff54727c2a4fba49cc91e05648
CatHub <165255694+CatHub027@users.noreply.github.com>
2024-05-24 12:05:14 +0700

    Update CatHubscript.lua

--- CatHubscript.lua
+++ CatHubscript.lua
@@ -1,16 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/slf0Dev/Ocerium_Project/main/Library.lua"))()
 Window = Library.Main("Cat Hub v2","LeftAlt") -- change "LeftAlt" to key that you want will hide gui
 
-local StarterGui = game:GetService("StarterGui")
-local assetID = "rbxasstid://11401835408"
-
-wait(4)
-StarterGui:SetCore("SendNotification", {
-    Title = "Cat Hub | Script",
-    Text = "Update to 5/22/2024"
-    Duration = 4
-})
-
 
 --//tab
 local Tab = Window.NewTab("Main")
