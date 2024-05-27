df9157acabc8899643658a820963193badb046cd
CatHub <165255694+CatHub027@users.noreply.github.com>
2024-05-27 16:36:19 +0700

    Update CatHubscript.lua

--- CatHubscript.lua
+++ CatHubscript.lua
@@ -1,6 +1,15 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("Cat Hub v0.5", "GrapeTheme")
 
+
+local Tab = Window:NewTab("Credit")
+local Section = Tab:NewSection("first to credit!")
+
+Section:NewLabel("Owner to memecat0")
+Section:NewLabel("Helper to alexhub")
+
+
+
 local Tab = Window:NewTab("main")
 local Section = Tab:NewSection("main")
 
