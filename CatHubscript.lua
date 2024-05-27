a04e01177e2be40ab2eb0c22b1694a84b76a3eee
CatHub <165255694+CatHub027@users.noreply.github.com>
2024-05-27 16:06:42 +0700

    Update CatHubscript.lua

--- CatHubscript.lua
+++ CatHubscript.lua
@@ -1,1 +1,9 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("TITLE", "DarkTheme")
 
+local Tab = Window:NewTab("Test")
+local Section = Tab:NewSection("TEST LOL")
+
+Section:NewButton("test", "test", function()
+    print("hello world")
+end)
