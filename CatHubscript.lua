401bc7620e29087adc6b2e9a572cd68e0f2d41eb
CatHub <165255694+CatHub027@users.noreply.github.com>
2024-05-28 14:56:26 +0700

    Update CatHubscript.lua

--- CatHubscript.lua
+++ CatHubscript.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
-local Window = Library.CreateLib("Cat Hub v0.5", "GrapeTheme")
+local Window = Library.CreateLib("Cat Hub v0.5", "Sentinel")
 
 
 local Tab = Window:NewTab("Credit")
