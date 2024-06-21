80c55e870f28254e35048defbde42b211646bdab
CatHub <165255694+CatHub027@users.noreply.github.com>
2024-06-21 09:53:05 +0700

    Update CatHub-Script-v1.lua

--- CatHub-Script-v1.lua
+++ CatHub-Script-v1.lua
@@ -1,1 +1,26 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/slf0Dev/Ocerium_Project/main/Library.lua"))()
+Window = Library.Main("Your Text","LeftAlt") -- change "LeftAlt" to key that you want will hide gui
 
+
+--//tab
+local Tab = Window.NewTab("Your Tab Text")
+
+
+--//section
+local Section = Tab.NewSection("Section Text")
+
+
+--// Button
+local Button = Section.NewButton("Button Text",function()
+-- code here
+end)
+
+
+--//Toggles
+local EnabledToggle = Section.NewToggle("Enabled Toggle",function(bool)
+-- code here
+end,true) -- "true" is the default value of toggle
+
+local DisabledToggle = Section.NewToggle("Disabled Toggle",function(bool)
+-- code here
+end,false) -- "false" is the default value of toggle
