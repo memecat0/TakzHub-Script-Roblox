a490bc41b294f1ef63060d0c7f9a391bccc2f41e
CatHub <165255694+CatHub027@users.noreply.github.com>
2024-07-02 21:53:45 +0700

    Update CatHub-Script-v1.lua

--- CatHub-Script-v1.lua
+++ CatHub-Script-v1.lua
@@ -2,34 +2,27 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/slf0D
 Window = Library.Main("Cat Hub v1","LeftAlt") -- change "LeftAlt" to key that you want will hide gui
 
 
---//tab
 local Tab = Window.NewTab("Credit")
 
 
---//section
 local Section = Tab.NewSection("first its can credits!")
 
 
---// Button
 local Button = Section.NewButton("Owner: Cat Hub",function()
 end)
 
 
---// Button
 local Button = Section.NewButton("Admin Respect: ???",function()
 end)
 
 
 
---//tab
 local Tab = Window.NewTab("Main")
 
 
---//section
 local Section = Tab.NewSection("Main")
 
 
---// Button
 local Button = Section.NewButton("infinite yield",function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end)
