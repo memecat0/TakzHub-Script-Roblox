235dc19e950272b920fb046fa36c9842fbcce398
CatHub <165255694+CatHub027@users.noreply.github.com>
2024-07-15 20:47:03 +0700

    Update CatHubscript.lua

--- CatHubscript.lua
+++ CatHubscript.lua
@@ -1,6 +1,7 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("Cat Hub v0.5", "Ocean")
 
+
 local Tab = Window:NewTab("Credit")
 local Section = Tab:NewSection("first to credit!")
 
