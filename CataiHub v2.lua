ffdb55c49fc07b0751ddb9b2ab003d3ea49522b8
CataiHub <165255694+CataiHub@users.noreply.github.com>
2024-08-14 06:02:48 +0700

    Update CataiHub v2.lua

--- CataiHub v2.lua
+++ CataiHub v2.lua
@@ -1,7 +1,17 @@
 local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
 local window = DrRayLibrary:Load("CataiHub v2", "Default")
 
-local tab = DrRayLibrary.newTab("My Tab", "ImageIdHere")
+local tab = DrRayLibrary.newTab("Main", "ImageIdHere")
+
+tab.newToggle("Test", "hi", true, function(toggleState)
+    if toggleState then
+        print("On")
+    else
+        print("Off")
+    end
+end)
+
+local tab = DrRayLibrary.newTab("Script", "ImageIdHere")
 
 tab.newButton("click to button", "test!", function()
     print('test!')
