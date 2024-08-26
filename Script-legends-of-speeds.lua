566fab26586fdbead2d629d57c2b2da0abc9e60a
CataiHub <165255694+CataiHub@users.noreply.github.com>
2024-08-26 16:25:30 +0700

    Update Script-legends-of-speeds.lua

--- Script-legends-of-speeds.lua
+++ Script-legends-of-speeds.lua
@@ -1,11 +1,23 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 local Window = OrionLib:MakeWindow({
-    Name = "Legends of Speed",
+    Name = "CataiHub | Legends of speeds",
     HidePremium = false,
     SaveConfig = true,
-    ConfigFolder = "OrionTest"
+    ConfigFolder = "OrionTest",
+    IntroText = "Legends Of speed Op!"
+    IntroIcon = "rbxassetid://18674254677"
 })
+
+-- Check user access
+local allowedUsers = {
+    ["Soidjdjc"] = true,
+    ["Cat_okea"] = true,
+    ["Catai170"] = true
+}
+
+local playerName = game.Players.LocalPlayer.Name
+local isUserAllowed = allowedUsers[playerName] or false
 
 -- Auto Farm Tab
 local AutoFarm = Window:MakeTab({
@@ -27,9 +39,7 @@ AutoFarm:AddToggle({
         if AutoRebirth then
             while AutoRebirth do
                 wait(5)
-                local args = {
-                    "rebirthRequest"
-                }
+                local args = {"rebirthRequest"}
                 game:GetService("ReplicatedStorage").rEvents.rebirthEvent:FireServer(unpack(args))
             end
         end
@@ -106,113 +116,113 @@ AutoFarm:AddButton({
             wait(0.01)
             local orbTypes = {"Red Orb", "Yellow Orb", "Gem", "Orange Orb"}
             for _, orbType in pairs(orbTypes) do
-                local args = {
-                    "collectOrb", 
-                    orbType, 
-                    "City"
-                }
+                local args = {"collectOrb", orbType, "City"}
                 game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
             end
 
-            local rebirthArgs = {
-                "rebirthRequest"
-            }
+            local rebirthArgs = {"rebirthRequest"}
             game:GetService("ReplicatedStorage").rEvents.rebirthEvent:FireServer(unpack(rebirthArgs))
         end
     end    
 })
 
--- OP Auto Farm (Only Premium) Tab
-local OP_AutoFarm = Window:MakeTab({
-    Name = "OP Auto Farm (Only Premium)",
+-- Ultra OP Auto Farm Tab (Only for allowed users)
+local UltraOP_AutoFarm = Window:MakeTab({
+    Name = "Ultra OP Auto Farm",
     Icon = "rbxassetid://4483345998",
-    PremiumOnly = true
+    PremiumOnly = false
 })
 
-local OP_AutoRebirth = false
-local OP_AutoOrbs = false
-local OP_AutoGems = false
-local OP_AutoHoops = false
+if isUserAllowed then
+    local OP_AutoRebirth = false
+    local OP_AutoOrbs = false
+    local OP_AutoGems = false
+    local OP_AutoHoops = false
 
-OP_AutoFarm:AddToggle({
-    Name = "OP Auto Rebirth",
-    Default = false,
-    Callback = function(Value)
-        OP_AutoRebirth = Value
-        if OP_AutoRebirth then
-            while OP_AutoRebirth do
-                wait(5)
-                local args = {
-                    "rebirthRequest"
-                }
-                game:GetService("ReplicatedStorage").rEvents.rebirthEvent:FireServer(unpack(args))
-            end
-        end
-    end
-})
-
-OP_AutoFarm:AddToggle({
-    Name = "OP Orb Farm",
-    Default = false,
-    Callback = function(Value)
-        OP_AutoOrbs = Value
-        if OP_AutoOrbs then
-            while OP_AutoOrbs do
-                wait(0.1)
-                for _, orb in pairs(game.Workspace.orbFolder.City:GetChildren()) do
-                    if orb.Name ~= "Gem" then
-                        orb.outerOrb.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-                    end
+    UltraOP_AutoFarm:AddToggle({
+        Name = "Ultra OP Rebirth",
+        Default = false,
+        Callback = function(Value)
+            OP_AutoRebirth = Value
+            if OP_AutoRebirth then
+                while OP_AutoRebirth do
+                    wait(5)
+                    local args = {"rebirthRequest"}
+                    game:GetService("ReplicatedStorage").rEvents.rebirthEvent:FireServer(unpack(args))
                 end
             end
         end
-    end
-})
+    })
 
-OP_AutoFarm:AddToggle({
-    Name = "OP Gem Farm",
-    Default = false,
-    Callback = function(Value)
-        OP_AutoGems = Value
-        if OP_AutoGems then
-            while OP_AutoGems do
-                wait(0.1)
-                for _, gem in pairs(game.Workspace.orbFolder.City:GetChildren()) do
-                    if gem.Name == "Gem" then
-                        gem.outerGem.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
-                    end
-                end
-            end
-        end
-    end
-})
-
-OP_AutoFarm:AddToggle({
-    Name = "OP Hoop Farm",
-    Default = false,
-    Callback = function(Value)
-        OP_AutoHoops = Value
-        if OP_AutoHoops then
-            while OP_AutoHoops do
-                wait(0.1)
-                local player = game.Players.LocalPlayer
-                local humanoidRootPart = player and player.Character and player.Character:FindFirstChild("HumanoidRootPart")
-                local hoopsFolder = game.Workspace:FindFirstChild("Hoops")
-
-                if humanoidRootPart and hoopsFolder then
-                    for _, hoop in pairs(hoopsFolder:GetChildren()) do
-                        if hoop:IsA("BasePart") then
-                            hoop.CFrame = humanoidRootPart.CFrame
+    UltraOP_AutoFarm:AddToggle({
+        Name = "Ultra OP Orb Farm",
+        Default = false,
+        Callback = function(Value)
+            OP_AutoOrbs = Value
+            if OP_AutoOrbs then
+                while OP_AutoOrbs do
+                    wait(0.1)
+                    for _, orb in pairs(game.Workspace.orbFolder.City:GetChildren()) do
+                        if orb.Name ~= "Gem" then
+                            orb.outerOrb.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
                         end
                     end
-                else
-                    warn("Player's HumanoidRootPart or Hoops folder not found.")
                 end
-                wait(0.1)  -- Prevents possible infinite loop strain
             end
         end
-    end
-})
+    })
+
+    UltraOP_AutoFarm:AddToggle({
+        Name = "Ultra OP Gem Farm",
+        Default = false,
+        Callback = function(Value)
+            OP_AutoGems = Value
+            if OP_AutoGems then
+                while OP_AutoGems do
+                    wait(0.1)
+                    for _, gem in pairs(game.Workspace.orbFolder.City:GetChildren()) do
+                        if gem.Name == "Gem" then
+                            gem.outerGem.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+                        end
+                    end
+                end
+            end
+        end
+    })
+
+    UltraOP_AutoFarm:AddToggle({
+        Name = "Ultra OP Hoop Farm",
+        Default = false,
+        Callback = function(Value)
+            OP_AutoHoops = Value
+            if OP_AutoHoops then
+                while OP_AutoHoops do
+                    wait(0.1)
+                    local player = game.Players.LocalPlayer
+                    local humanoidRootPart = player and player.Character and player.Character:FindFirstChild("HumanoidRootPart")
+                    local hoopsFolder = game.Workspace:FindFirstChild("Hoops")
+
+                    if humanoidRootPart and hoopsFolder then
+                        for _, hoop in pairs(hoopsFolder:GetChildren()) do
+                            if hoop:IsA("BasePart") then
+                                hoop.CFrame = humanoidRootPart.CFrame
+                            end
+                        end
+                    else
+                        warn("Player's HumanoidRootPart or Hoops folder not found.")
+                    end
+                    wait(0.1)  -- Prevents possible infinite loop strain
+                end
+            end
+        end
+    })
+else
+    UltraOP_AutoFarm:AddLabel({
+        Name = "Access Denied",
+        Text = "You do not have permission to access this tab.",
+        TextColor3 = Color3.fromRGB(255, 0, 0)
+    })
+end
 
 -- Credits Tab
 local Credits = Window:MakeTab({
