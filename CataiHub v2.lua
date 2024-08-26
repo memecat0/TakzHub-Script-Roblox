7d00240cb56aa7fa0ea84f54c3a0ab1efa7fe4cf
CataiHub <165255694+CataiHub@users.noreply.github.com>
2024-08-26 09:53:45 +0700

    Update CataiHub v2.lua

--- CataiHub v2.lua
+++ CataiHub v2.lua
@@ -1,13 +1,13 @@
 local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
-local window = DrRayLibrary:Load("CataiHub v2", "Default")
+local window = DrRayLibrary:Load("CataiHub v2", "rbxassetid://18674254677")
 
-local tab = DrRayLibrary.newTab("Main", "ImageIdHere")
+local tab = DrRayLibrary.newTab("Main", "rbxassetid://18675218518")
 
 tab.newButton("INF YIELD", "FE BUT INF YIELD CMDS +150!", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end)
 
-local tab = DrRayLibrary.newTab("Script", "ImageIdHere")
+local tab = DrRayLibrary.newTab("Script", "rbxassetid://18675246284")
 
 tab.newButton("RedzHub", "RedzHub from Blox fruit games", function()
    loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/BloxFruits/main/redz9999"))()
