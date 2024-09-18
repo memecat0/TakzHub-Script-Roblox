abec021a9c9646cae38b61f0339ca0a026f4511e
CataiHub <165255694+CataiHub@users.noreply.github.com>
2024-09-18 10:19:43 +0700

    Update takzhub-v1.lua

--- takzhub-v1.lua
+++ takzhub-v1.lua
@@ -89,7 +89,7 @@ Toggle.Name = "Toggle"
 Toggle.Parent = ScreenGui
 Toggle.BackgroundTransparency = 1
 Toggle.Position = UDim2.new(0, 0, 0.454706937, 0)
-Toggle.Size = UDim2.new(0, 35, 0, 35)
+Toggle.Size = UDim2.new(0, 45, 0, 45)
 Toggle.Image = "rbxassetid://72444682975876"
 Toggle.Draggable = true
 
