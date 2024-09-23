8525541bdc44a142d6ccf555bac389cff0374e6b
memecat0 <165255694+memecat0@users.noreply.github.com>
2024-09-23 11:34:44 +0700

    Update TakzHubScript-v1.5.lua

--- TakzHubScript-v1.5.lua
+++ TakzHubScript-v1.5.lua
@@ -1,5 +1,57 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Name = "ScreenGui"
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+ScreenGui.ResetOnSpawn = false
+
+local Toggle = Instance.new("ImageButton")
+Toggle.Name = "Toggle"
+Toggle.Parent = ScreenGui
+Toggle.BackgroundTransparency = 1
+Toggle.Position = UDim2.new(0, 0, 0.454706937, 0)
+Toggle.Size = UDim2.new(0, 45, 0, 45)
+Toggle.Image = "rbxassetid://72444682975876"
+Toggle.Draggable = true
+
+local Corner = Instance.new("UICorner")
+Corner.CornerRadius = UDim.new(0.2, 0)
+Corner.Parent = Toggle
+
+local UIStroke = Instance.new("UIStroke")
+UIStroke.Parent = Toggle
+UIStroke.Thickness = 2
+UIStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+
+local isOn = false
+
+local function setRainbowStroke()
+    while true do
+        for i = 0, 1, 0.01 do
+            UIStroke.Color = Color3.fromHSV(i, 1, 1)
+            wait(0.1)
+        end
+    end
+end
+
+coroutine.wrap(setRainbowStroke)()
+
+local function toggleOrion()
+    if gethui():FindFirstChild("Fluent") then
+        gethui().Orion.Enabled = not gethui().Fluent.Enabled
+    end
+end
+
+Toggle.MouseButton1Click:Connect(function()
+    isOn = not isOn
+    if isOn then
+        Toggle.Image = "rbxassetid://72444682975876"
+    else
+        Toggle.Image = "rbxassetid://72444682975876"
+    end
+    toggleFluent()
+end)
+
 local Window = Fluent:CreateWindow({
     Title = "TakzHub " .. Fluent.Version,
     SubTitle = "Script",
