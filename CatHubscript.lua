dd850db4691506a85a624f89acf4adbba4e2301d
memecat0 <165255694+memecat0@users.noreply.github.com>
2024-10-06 09:35:20 +0700

    Update CatHubscript.lua

--- CatHubscript.lua
+++ CatHubscript.lua
@@ -1,59 +1,6 @@
 local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
 local Window = Library.CreateLib("TakzHub v0.5", "Ocean")
 
-repeat task.wait(0.25) until game:IsLoaded()
-
-getgenv().Image = "rbxassetid://72444682975876"
-getgenv().ToggleUI = "L"
-
-task.spawn(function()
-    if not getgenv().LoadedMobileUI then
-        getgenv().LoadedMobileUI = true
-
-        -- Create the ScreenGui
-        local OpenUI = Instance.new("ScreenGui")
-        OpenUI.Name = "OpenUI"
-        OpenUI.Parent = game:GetService("CoreGui")
-        OpenUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-        -- Create the ImageButton
-        local ImageButton = Instance.new("ImageButton")
-        ImageButton.Parent = OpenUI
-        ImageButton.BackgroundColor3 = Color3.fromRGB(105, 105, 105)
-        ImageButton.BackgroundTransparency = 0.8
-        ImageButton.Position = UDim2.new(0.9, 0, 0.1, 0)
-        ImageButton.Size = UDim2.new(0, 45, 0, 45)
-        ImageButton.Image = getgenv().Image
-        ImageButton.Draggable = true
-
-        -- Create rounded corners
-        local UICorner = Instance.new("UICorner")
-        UICorner.CornerRadius = UDim.new(0, 10) -- Adjust this value for more or less rounding
-        UICorner.Parent = ImageButton
-
-        -- Create the UIStroke for the rainbow effect
-        local UIStroke = Instance.new("UIStroke")
-        UIStroke.Parent = ImageButton
-        UIStroke.Thickness = 2
-        UIStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-
-        -- Rainbow stroke effect
-        task.spawn(function()
-            while true do
-                for i = 0, 1, 0.01 do
-                    UIStroke.Color = Color3.fromHSV(i, 1, 1)
-                    task.wait(0.1)
-                end
-            end
-        end)
-
-        -- Button click event to toggle the Fluent UI
-        ImageButton.MouseButton1Click:Connect(function()
-            game:GetService("VirtualInputManager"):SendKeyEvent(true, getgenv().ToggleUI, false, game)
-        end)
-    end
-end)
-
 local Tab = Window:NewTab("Credit")
 local Section = Tab:NewSection("first to credit!")
 
