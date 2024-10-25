ff185daadb8b0036a3b87dc121549182c518f340
memecat0 <165255694+memecat0@users.noreply.github.com>
2024-10-25 15:34:35 +0700

    Create Fps cap unlocker.lua

---
+++ Fps cap unlocker.lua
@@ -0,0 +1,53 @@
+local RunService = game:GetService("RunService")
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local FPSCap = 6500
+local function SetFPSCap(fps)
+    setfpscap(fps)
+end
+
+local function AutoAntiLag()
+    if RunService:IsClient() then
+        settings().Rendering.QualityLevel = Enum.QualityLevel.Level01
+    end
+end
+
+SetFPSCap(FPSCap)
+AutoAntiLag()
+
+RunService.Heartbeat:Connect(function()
+    AutoAntiLag()
+end)
+
+local Window = OrionLib:MakeWindow({Name = "Performance Settings", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionConfig"})
+
+local Tab = Window:MakeTab({
+    Name = "FPS Settings",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
+})
+
+Tab:AddSlider({
+    Name = "FPS Cap",
+    Min = 30,
+    Max = 5000,
+    Default = FPSCap,
+    Color = Color3.fromRGB(255, 255, 255),
+    Increment = 10,
+    ValueName = "FPS",
+    Callback = function(value)
+        SetFPSCap(value)
+    end    
+})
+
+Tab:AddToggle({
+    Name = "Auto Anti-Lag",
+    Default = true,
+    Callback = function(value)
+        if value then
+            AutoAntiLag()
+        end
+    end    
+})
+
+OrionLib:Init()
