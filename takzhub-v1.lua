105b6a49d94200f097ccc01f63e5168d95a89bba
memecat0 <165255694+memecat0@users.noreply.github.com>
2024-10-25 09:43:16 +0700

    Update takzhub-v1.lua

--- takzhub-v1.lua
+++ takzhub-v1.lua
@@ -1,1 +1,22 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "TakzHub Script V2", HidePremium = false, SaveConfig = true, ConfigFolder = "TakzHub"})
 
+OrionLib:MakeNotification({
+	Name = "Welcome!",
+	Content = "enjoy.",
+	Image = "rbxassetid://4483345998",
+	Time = 3
+})
+
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+	Name = "Button!",
+	Callback = function()
+      		print("button pressed")
+  	end    
+})
